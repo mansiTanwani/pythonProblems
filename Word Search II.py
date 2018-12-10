2852c2faad18139e265f06231c5430023ce52e8f
mansiTanwani <45731245+mansiTanwani@users.noreply.github.com>
2018-12-10 06:44:31 +0530

    Create Word Search II.py

---
+++ Word Search II.py
@@ -0,0 +1,50 @@
+class Solution:
+    
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        
+        
+        def findWords(self, board, words):
+            result=[]
+            final_output=[]
+            letter=0
+            index_adj = [[0,1],[1,0],[0,-1],[-1,0]]
+            
+            def look_adj(i,j,letter):
+                for row, col in index_adj:
+                    try:
+                        if word[letter]==board[i+row][j+col]:
+                            result.append(word[letter])
+
+                            if len(result)==len(word): 
+                                str1 = ''.join(result)
+                                final_output.append(str1)
+                            else:
+                                look_adj(i+row, j+col, letter+1)
+
+                    except:
+                        continue
+
+            for word in words:
+                result=[]
+                for i in range(0,len(board)):
+                    for j in range(0, len(board[i])):
+
+                        letter=0
+                        if board[i][j]==word[0]:
+                            if word[0] not in result:
+                                result.append(word[0])
+                            letter = letter+1
+                            look_adj(i,j,letter)
+
+
+            return final_output
+       
+         
+                
+    
+
+    
